a9e67bb4fd25d92d69fecd13f93eddecbcb06626
yao <yaopaine@outlook.com>
2018-01-29 10:06:08 +0800

    单一职责

--- app/src/main/java/com/yaopaine/designpattern/srp/chapter2/ImageCache.java
+++ app/src/main/java/com/yaopaine/designpattern/srp/chapter2/ImageCache.java
@@ -12,7 +12,7 @@ import android.util.LruCache;
 
 public class ImageCache {
 
-    private LruCache<String, Bitmap> mImageCahce;
+    private LruCache<String, Bitmap> mImageCache;
 
     public ImageCache() {
         initCache();
@@ -21,11 +21,19 @@ public class ImageCache {
     private void initCache() {
         long l = Runtime.getRuntime().maxMemory() / 1024;
         int maxMemory = (int) (l / 4);
-        mImageCahce = new LruCache<String, Bitmap>(maxMemory) {
+        mImageCache = new LruCache<String, Bitmap>(maxMemory) {
             @Override
             protected int sizeOf(String key, Bitmap value) {
                 return value.getRowBytes() * value.getHeight() / 1024;
             }
         };
+    }
+
+    public void putBitmap(String url, Bitmap bitmap) {
+        mImageCache.put(url, bitmap);
+    }
+
+    public Bitmap getBitmap(String url) {
+        return mImageCache.get(url);
     }
 }
--- app/src/main/java/com/yaopaine/designpattern/srp/chapter2/ImageLoader.java
+++ app/src/main/java/com/yaopaine/designpattern/srp/chapter2/ImageLoader.java
@@ -1,5 +1,16 @@
 package com.yaopaine.designpattern.srp.chapter2;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @Description
  * @AuthorCreated yaopaine
@@ -8,5 +19,46 @@ package com.yaopaine.designpattern.srp.chapter2;
  */
 
 public class ImageLoader {
-    
+
+    private ImageCache mImageCache = new ImageCache();
+    private ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public ImageLoader() {
+
+    }
+
+    public void displayBitmap(final String url, final ImageView imageView) {
+        imageView.setTag(imageView.getId(), url);
+        Bitmap bitmap = mImageCache.getBitmap(url);
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        mExecutorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap downloadBitmap = downloadBitmap(url);
+                if (downloadBitmap == null) return;
+                mImageCache.putBitmap(url, downloadBitmap);
+                if (url.equals(imageView.getTag(imageView.getId()))) {
+                    imageView.setImageBitmap(downloadBitmap);
+                }
+            }
+        });
+    }
+
+    public Bitmap downloadBitmap(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            Bitmap bitmap = BitmapFactory.decodeStream(urlConnection.getInputStream());
+            urlConnection.disconnect();
+            return bitmap;
+        } catch (MalformedURLException e) {
+
+        } catch (IOException e) {
+
+        }
+        return null;
+    }
 }
